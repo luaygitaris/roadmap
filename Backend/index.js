f05282b4c6db3f8de05792636f06b74a082d79d8
luaygitaris <luaygitaris.com>
2025-04-28 08:05:43 +0700

    menambahakan router project dan skilss

--- Backend/index.js
+++ Backend/index.js
@@ -10,15 +10,15 @@ app.use("/api/categories", require("./Routes/routesCategory"));
 app.use("/api/materials", require("./Routes/routesMateri"));
 app.use("/api/login", require("./Routes/routesLogin"));
 app.use("/api/progress", require("./Routes/routesProgress"));
+app.use("/api/skills", require("./Routes/routesSkills"));
+app.use("/api/projects", require("./Routes/routesProjects"));
 
 app.get('/', (req, res) => {
   res.send('API Working');
 });
 
-// Export the Express app for Vercel
 module.exports = app;
 
-// Only listen when not in Vercel environment
 if (process.env.VERCEL !== '1') {
   app.listen(port, () => {
     console.log(`Server berjalan di http://localhost:${port}`);
